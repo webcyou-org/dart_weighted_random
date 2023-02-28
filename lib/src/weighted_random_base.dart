d53502c9275e65d0c0e7117468039c58211a6016
panicdragon <d.takayama.web@gmail.com>
2023-03-01 01:19:44 +0900

    feat: 🎸 first commit

---
+++ lib/src/weighted_random_base.dart
@@ -0,0 +1,30 @@
+import 'dart:math' as math;
+
+Map<String, dynamic> weightedRandom<T>(List<T> items, List<int> weights) {
+    if (items.length != weights.length) {
+      throw 'Items and weights must be of the same size';
+    }
+
+    if (items.isEmpty) {
+      throw 'Items must not be empty';
+    }
+
+    List<int> cumulativeWeights = [];
+    for (int i = 0; i < weights.length; i++) {
+      cumulativeWeights.add(weights[i] + (i == 0 ? 0 : cumulativeWeights[i - 1]));
+    }
+
+    final maxCumulativeWeight = cumulativeWeights[cumulativeWeights.length - 1];
+    final randomNumber = maxCumulativeWeight * math.Random().nextDouble();
+    Map<String, dynamic> result = {};
+
+    for (int itemIndex = 0; itemIndex < items.length; itemIndex++) {
+      if (cumulativeWeights[itemIndex] >= randomNumber) {
+        return result = {
+          'item': items[itemIndex],
+          'index': itemIndex,
+        };        
+      }
+    }
+    return result;
+}
---
+++ lib/weighted_random.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library weighted_random;
+
+export 'src/weighted_random_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
