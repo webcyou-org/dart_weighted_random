8a40af830ffa16b2118863e10774196e76357a43
panicdragon <d.takayama.web@gmail.com>
2023-03-01 01:47:03 +0900

    refactor: 💡 add test

--- example/weighted_random_example.dart
+++ example/weighted_random_example.dart
@@ -1,7 +1,7 @@
 import 'package:weighted_random/weighted_random.dart';
 
 void main() {
-  final List<String> stringList = ["a","b", "c", "d"];
+  final List<String> stringList = ["a", "b", "c", "d"];
   final Map<String, dynamic> result = weightedRandom<String>(stringList, [40, 20, 60, 20]);
   
   print(result);
--- test/weighted_random_test.dart
+++ test/weighted_random_test.dart
@@ -3,14 +3,16 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      final List<String> stringList = ["a", "b", "c", "d"];      
+      final Map<String, dynamic> result = weightedRandom<String>(stringList, [0, 0, 1, 0]);
+
+      expect(result['index'], 2);
+      expect(result['item'], 'c');
     });
   });
 }
